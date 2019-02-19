3ae24957bd7deabd02ffca32f02b4214ad6a4db4
Trai Lynne <trailynne@gmail.com>
2019-02-18 20:55:43 -0500

    Projects: Defined GET request handler for CREATE operation success => 200 with new project incomplete => 400 with error message internal error => 500 with error message

--- routers/projectRouter.js
+++ routers/projectRouter.js
@@ -1,17 +1,62 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('../data/helpers/projectModel');
 
 router.use(express.json())
 
 // C - Create
-router.post('/', (req, res) => {
-    res
-        .status(201)
-        .json({
-            operation: 'POST',
-            url: '/api/projects/'
-        });
+router.post('/', async (req, res) => {
+    const newRecord = req.body;
+
+    try {
+        if (!newRecord.name || newRecord.name === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide a name for the new record'
+                });
+        } else if (!newRecord.description || newRecord.description === '') {
+            res
+                .status(400)
+                .json({
+                    errorMessage: 'Please provide a description for the new record'
+                });
+        } else if (!newRecord.completed) {
+            newRecord.completed = false;
+
+            const newProject = await db.insert(newRecord);
+
+            newProject ?
+                res
+                    .status(201)
+                    .json(newProject)
+                :
+                res
+                    .status(500)
+                    .json({
+                        errorMessage: 'There was an error processing your request'
+                    });
+        } else {
+            const newProject = await db.insert(newRecord);
+
+            newProject ?
+                res
+                    .status(201)
+                    .json(newProject) :
+                res
+                    .status(500)
+                    .json({
+                        errorMessage: 'There was an error processing your request'
+                    });
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                errorMessage: 'Houston, we have a problem'
+            });
+    }
 });
 
 // R - Read
