a799a2594128ef687ae22993c1470de44284423b
Trai Lynne <trailynne@gmail.com>
2019-02-18 20:20:41 -0500

    Set up sub routers Imported express imported subRouters to main router

--- routers/index.js
+++ routers/index.js
@@ -2,6 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
+// Sub-Routers
+const actionRouter = require('./actionRouter');
+const projectRouter = require('./projectRouter');
+
+router.use('/actions', actionRouter)
+router.use('/projects', projectRouter);
+
 router.use('/', (req, res) => res.send('Welcome to the main API'));
 
 module.exports = router;
