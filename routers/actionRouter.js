642ab7be76645651fbbdb5393403d5426d89694e
Trai Lynne <trailynne@gmail.com>
2019-02-18 22:06:46 -0500

    Actions: Defined POST request handler for CREATE operation success => 201 with new action incomplete => 400 with error message internal error => 500 with error message

--- routers/actionRouter.js
+++ routers/actionRouter.js
@@ -1,21 +1,82 @@
 const express = require('express');
 
 const router = express.Router();
+const db = require('../data/helpers/actionModel');
+const projectDb = require('../data/helpers/projectModel');
 
 router.use(express.json())
 
 // C - Create
-router.post('/', (req, res) => {
-    res
-        .status(201)
-        .json({
-            operation: 'POST',
-            url: '/api/actions/'
-        });
+router.post('/new/:projectId', async (req, res) => {
+    const newRecord = req.body;
+    const { projectId } = req.params;
+
+    try {
+        const project = await projectDb.get(projectId);
+
+        if (project) {
+            
+            if (!newRecord.notes || newRecord.notes === '') {
+                res
+                    .status(400)
+                    .json({
+                        errorMessage: 'Please provide notes for the new record'
+                    });
+            } else if (!newRecord.description || newRecord.description === '') {
+                res
+                    .status(400)
+                    .json({
+                        errorMessage: 'Please provide a description for the new record'
+                    });
+            } else if (!newRecord.completed) {
+                newRecord.completed = false;
+                newRecord.project_id = projectId
+    
+                const newProject = await db.insert(newRecord);
+    
+                newProject ?
+                    res
+                    .status(201)
+                    .json(newProject) :
+                    res
+                    .status(500)
+                    .json({
+                        errorMessage: 'There was an error processing your request'
+                    });
+            } else {
+                newRecord.project_id = projectId
+
+                const newProject = await db.insert(newRecord);
+                
+                newProject ?
+                    res
+                    .status(201)
+                    .json(newProject) :
+                    res
+                    .status(500)
+                    .json({
+                        errorMessage: 'There was an error processing your request'
+                    });
+            }
+        } else {
+            res
+                .status(404)
+                .json({
+                    errorMessage: 'Action must be attached to an existing record'
+                });
+        }
+    } catch (err) {
+        res
+            .status(500)
+            .json({
+                errorMessage: 'Houston, we have a problem'
+            });
+    }
 });
 
 // R - Read
 router.get('/', (req, res) => {
+    
     res
         .status(200)
         .json({
