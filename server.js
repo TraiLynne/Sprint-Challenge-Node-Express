3c8dbcc429226d5b1eaa06238478fec7030fa0ec
Trai Lynne <trailynne@gmail.com>
2019-02-18 20:11:08 -0500

    Set Up Main Router imported express defined router with Express Router method Imported to server.js

--- server.js
+++ server.js
@@ -4,6 +4,17 @@ const helmet = require('helmet');
 const server = express();
 const port = 4000;
 
+const mainRouter = require('./routers');
+
+// Middleware
 server.use(helmet());
 
+
+// Main Router
+server.use('/api', mainRouter)
+
+server.use('/', (req, res) => res.send('It\'s working !! \nIt\'s working'));
+
+
+// Listener
 server.listen(port, () => console.log(`Server listening on port: ${port}`));
